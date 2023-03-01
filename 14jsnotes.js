6be6711d83a620c4e9356612203c84cfbc66ad39
willmino <will.mino@yahoo.com>
2023-03-01 12:11:28 -0800

    uploading files

---
+++ 14jsnotes.js
@@ -0,0 +1,32 @@
+/*
+
+14.2.2
+
+Creating a feature branch
+
+git pull origin main lets you know that your version of the main branch on the computer is up to date with the github main branch.
+
+Its a best practice to either pull from the main branch to get latest updates to main branch.
+
+OR
+
+Pull "upstream" of the branch you are creating. Important because version of the main branch on your computer needs to be up to date with the main branch on GitHub.
+
+
+CREATING A BRANCH STEPS
+
+1. Navigate to repo folder in terminal. type git pull or git pull origin main.
+
+2. git checkout -b Simple_Leaflet_Map   .  Then press enter   (git checkout lets us navigate between branches).   -b indicates we are creating a new branch.
+                                                name of the new branch follows the -b
+
+Successfull branch creation will return :    Switched to a new branch 'Simple_Leaflet_Map'
+
+
+
+
+
+
+
+
+*/
