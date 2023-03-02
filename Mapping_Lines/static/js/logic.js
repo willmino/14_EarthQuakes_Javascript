92f92eba5210151e748375170d287cac41b88333
willmino <will.mino@yahoo.com>
2023-03-02 11:21:34 -0800

    uploading files

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,14 +2,17 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      34.0522, -118.2437
-    ],
-    zoom: 4
-  });
+let map = L.map('mapid').setView([37.6214, -122.3790], 5);
 // Get data from cities.js
 let cityData = cities;
+
+let line = [
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
+
 
 cityData.forEach(function(city){
   console.log(city)
@@ -20,12 +23,21 @@ cityData.forEach(function(city){
   .addTo(map);
 });
 
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}' ,{
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}' ,{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  weight:4,
+  opacity:0.5,
+  dashArray: '10,10'
+
+
+}).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
